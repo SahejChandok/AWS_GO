b90515c78e1d633195073d3316ef5edaf0d47c5c
SahejChandok <chandoksahej@gmail.com>
2025-01-07 17:06:55 -0500

    basic go application

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func greetingHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.WriteHeader(http.StatusOK)
+	fmt.Fprintf(rw, "Hello ")
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/greeting", greetingHandler).Methods("GET")
+
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+	s.ListenAndServe()
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+}
